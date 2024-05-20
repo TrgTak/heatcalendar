52fd0d902ffef36f55e9107be31853335d30e6d8
Turgut <gutak.turbulut@gmail.com>
2024-05-20 13:26:53 +0300

    Mistyped locale option value is fixed from "en" to "en-GB".

--- src/app/services/locale/locale.options.ts
+++ src/app/services/locale/locale.options.ts
@@ -1,7 +1,7 @@
 import { OptionModel } from "../../types/generic";
 
 export const LOCALES: OptionModel[] = [
-    { label: "UK", value: "en"},
+    { label: "UK", value: "en-GB"},
     { label: "US", value: "en-US"},
     { label: "FI", value: "fi"},
     { label: "IR", value: "fa"}
